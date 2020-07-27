18f8b2c6fbf16ea2d09bbad00303d57d2eea6f58
unknown <heykassio@live.com>
2020-07-27 15:05:41 -0300

    Setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import Data from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: '#141414'}}>
+      <Menu/>
+      <BannerMain 
+      videoTitle={Data.categorias[0].videos[0].titulo}
+      url={Data.categorias[0].videos[0].url}
+      videoDescription={"Uma das minhas músicas favoritas! Asthenia do blink me passa tanta energia, não importa o momento em que estou ouvindo."}
+      />
+      <Carousel 
+      ignoreFirstVideo
+      category={Data.categorias[0]}
+      />
+      <Carousel 
+      category={Data.categorias[1]}
+      />
+      <Carousel 
+      category={Data.categorias[2]}
+      />   
+      <Carousel
+      category={Data.categorias[3]}
+      />
+      <Carousel
+      category={Data.categorias[4]}
+      />
+      <Footer />  
     </div>
   );
 }
